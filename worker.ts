03b1179a1db03de5e6ce6553936d836860594b06
Taylor Reid <taylorsreid@gmail.com>
2025-04-05 01:49:02 -1000

    simplified worker control flow and reduced time cost

--- worker.ts
+++ worker.ts
@@ -3,12 +3,8 @@
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 // THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-// prevents TS errors
 declare var self: Worker;
-
 const AsyncFunction = async function () {}.constructor
-
-let isAsync: boolean
 let fn: Function
 
 // @ts-ignore
@@ -18,28 +14,18 @@ self.onmessage = async (event: MessageEvent) => {
         const argNames: string[] = funcString.substring(funcString.indexOf('(') + 1, funcString.indexOf(')')).split(',')
         const funcBody: string = funcString.substring(funcString.indexOf('{') + 1, funcString.length-1).trim()
         if (funcString.startsWith('async')) {
-            isAsync = true
             fn = AsyncFunction(...argNames, funcBody)
         }
         else {
-            isAsync = false
             fn = Function(...argNames, funcBody)
         }
     }
     else if (event.data.type === 'call') {
         try {
-            if (isAsync) {
-                postMessage({
-                    type: 'success',
-                    data: await fn.call(undefined, ...event.data.data)
-                })
-            }
-            else {
-                postMessage({
-                    type: 'success',
-                    data: fn.call(undefined, ...event.data.data)
-                })
-            }
+            postMessage({
+                type: 'success',
+                data: await fn.call(undefined, ...event.data.data)
+            })
         } catch (error) {
             postMessage({
                 type: 'failure',
