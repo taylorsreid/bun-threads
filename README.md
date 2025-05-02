3e81c736caf04fb89f06bb155156c660d18894b7
Taylor Reid <taylorsreid@gmail.com>
2025-05-01 16:07:08 -1000

    drop node support, move worker back to separate file

--- README.md
+++ README.md
@@ -1,15 +1,41 @@
 # bun-threads
 
-To install dependencies:
+## A TypeScript Library to Simplify Bun Worker Threads 🧵
+bun-threads is a lightweight, developer-friendly TypeScript library that makes it effortless to use the worker threads API for offloading heavy or asynchronous tasks. It wraps the worker thread API with a clean, promise-based interface and TypeScript support, enabling easy parallel processing without boilerplate or separate worker files. bun-threads makes writing multithreaded Javascript and Typescript feel more like writing the asynchronous single threaded code that you are used to writing.
 
-```bash
-bun install
-```
+## 🔧 Features
+- Simple API: Create and run worker threads using Thread and/or ThreadPool classes.
 
-To run:
+- Type-Safe: TypeScript support out of the box.
 
-```bash
-bun run index.ts
-```
+- Automatic Scaling: Manage worker lifecycle and scaling without verbose setup code.
 
-This project was created using `bun init` in bun v1.2.5. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
+- Low Config: Works out of the box, no setup files or separate worker files required.
+
+## FAQ
+### Why not just use one of the existing libraries that does this like multithreading.io, tinypool, poolifier, threads.js, etc?
+- They're great projects, but they either don't work in Bun, and/or they require that you to create a separate worker file.
+
+### What makes this library Bun specific? Will it work with Node?
+- TODO:
+- ~~At the moment this library isn't using any Bun specific APIs and should work with Node. The Bun Worker API is still experimental and still has some bugs in it that make not ready for production use. Once the Bun Worker API stabilizes, support for Node will be dropped.~~
+- Reverted to using Bun APIs since Node support not planned anymore.
+
+### I keep getting a ReferenceError: x is not defined error. How do I fix this?
+- Check your imports. Worker threads don't have access to your top level imports at the top of your file.
+Instead, use dynamic imports inside of your thread's callback function.
+- Example:
+  ```ts
+  import { availableParallelism } from "os";
+  const badThread = new Thread(() => {
+      console.log(availableParallelism())
+  })
+  badThread.run() // throws ReferenceError: availableParallelism is not defined
+
+  
+  const goodThread = new Thread(async () => {
+      const os = await import("os")
+      console.log(os.availableParallelism())
+  })
+  goodThread.run() // works correctly
+  ```
--- thread.ts
+++ thread.ts
@@ -227,48 +227,7 @@ export class Thread<T = any> extends EventEmitter {
 
             // check if the worker has closed, and if it has, create a new one and update the function
             if (typeof this.worker === 'undefined') {
-                this.worker = new Worker(URL.createObjectURL(new Blob(
-                    [
-                        `
-                        import { parentPort } from "worker_threads";
-                        
-                        const AsyncFunction = async function () { }.constructor
-                        let fn: Function
-                        
-                        parentPort?.on('message', async (event: any) => {
-                            if (event.action === 'set') {
-                                const argNames: string[] = event.data.substring(event.data.indexOf('(') + 1, event.data.indexOf(')')).split(',')
-                                const funcBody: string = event.data.substring(event.data.indexOf('{') + 1, event.data.length - 1).trim()
-                                if (event.data.startsWith('async')) {
-                                    fn = AsyncFunction(...argNames, funcBody)
-                                }
-                                else {
-                                    fn = Function(...argNames, funcBody)
-                                }
-                            }
-                            else if (event.action === 'call') {
-                                try {
-                                    parentPort?.postMessage({
-                                        id: event.id,
-                                        action: 'resolve',
-                                        data: await fn.call(undefined, ...event.data)
-                                    })
-                                } catch (error) {
-                                    parentPort?.postMessage({
-                                        id: event.id,
-                                        action: 'reject',
-                                        data: error instanceof ReferenceError ? new ReferenceError(error.message + \`.\\nThis is usually caused by referencing top level imports within your Thread's callback function.\\nOnly dynamic imports made inside of the Thread's callback function are supported.\\nPlease see the README for examples.\`) : error
-                                    })
-                                }
-                            }
-                        })
-                        `
-                    ],
-                    {
-                        type: "application/typescript"
-                    }
-                )))
-                // this.worker = new Worker("./worker")
+                this.worker = new Worker(import.meta.dirname + "/worker")
                 this.worker.postMessage({
                     action: 'set',
                     data: this.fn.toString()
@@ -276,7 +235,7 @@ export class Thread<T = any> extends EventEmitter {
             }
 
             // create a unique id for each request to ensure that each promise only resolves for the correct request
-            const id: string = crypto.randomUUID() // TODO: switch to Bun.randomUUIDv7() once the Bun workers API becomes stable and Node support is dropped
+            const id: string = Bun.randomUUIDv7()
 
             // function to check each message from the worker thread
             const check = async (event: any) => {
